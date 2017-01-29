dbfdc35393d488d43945f724ad12f29637645965
Vi Hoang Son <vihoangson@gmail.com>
2017-01-29 09:18:57 +0700

    update

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,17 +1,7 @@
 <?php
-use Devristo\BBCode\BBCode;
-use Devristo\BBCode\Parser\RenderContext;
-use Devristo\BBCode\Parser\BBDomElement;
-require_once "vendor/jbbcode/Parser.php";
-
 class MY_Controller extends CI_Controller{
     public function __construct()
     {
         parent::__construct();
-
-
-
-
-
     }
 }
